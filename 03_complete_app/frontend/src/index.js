c1796f41f6411409fc20f57acd5966e855b8132c
Dima Pristupa <dima@pristupa.dev>
2025-05-30 15:37:30 +0300

    https://github.com/kamu-data/kamu-cli/pull/1239

--- 03_complete_app/frontend/src/index.js
+++ 03_complete_app/frontend/src/index.js
@@ -1,30 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// NOTE: Everything that does not relate to the unit test within this file has been updated or commented out.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 import { BrowserProvider } from 'ethers';
 import { SiweMessage } from 'siwe';
 
-const domain = window.location.host;
-const origin = "http://localhost:4200/v/login";
+// const domain = window.location.host;
+const origin = "http://platform.example.com/v/login";
 const provider = new BrowserProvider(window.ethereum);
 
-const BACKEND_ADDR = "http://localhost:3000";
+// const BACKEND_ADDR = "http://localhost:3000";
 async function createSiweMessage(address, statement) {
-    const res = await fetch(`${BACKEND_ADDR}/nonce`, {
-        credentials: 'include',
-    });
-
-    const now = new Date();
-    const minutes_15_in_seconds = 15 * 60 * 1000;
+    // const res = await fetch(`${BACKEND_ADDR}/nonce`, {
+    //     credentials: 'include',
+    // });
+    //
+    // const minutes_15_in_seconds = 15 * 60 * 1000;
     const message = new SiweMessage({
         // NOTE: for some reason, the "scheme" field breaks a parsing on the backend side.
         // scheme,
-        domain,
+        domain: 'platform.example.com',
         address,
         statement,
         uri: origin,
         version: '1',
-        chainId: (await provider.getNetwork()).chainId,
-        nonce: await res.text(),
-        issuedAt: now.toISOString(),
-        expirationTime: new Date(now.getTime() + minutes_15_in_seconds).toISOString(),
+        chainId: '1',
+        nonce: 'FROZEN1NONCE1TEST',
+        issuedAt: new Date('2050-01-02T03:04:05Z').toISOString(),
+        expirationTime: new Date('2050-01-02T03:19:05Z').toISOString(),
     });
     return message.prepareMessage();
 }
@@ -44,6 +49,7 @@ async function signInWithEthereum() {
     const signature = await signer.signMessage(message);
 
     console.log(`message:\n${message}\n`);
+    console.log(`signature:\n${signature}\n`);
 
     console.log('GQL request body (for copy&paste):');
     console.log(`
@@ -59,27 +65,27 @@ mutation {
 }
     `);
 
-    const res = await fetch(`${BACKEND_ADDR}/verify`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message, signature }),
-        credentials: 'include'
-    });
-    console.log(await res.text());
+    // const res = await fetch(`${BACKEND_ADDR}/verify`, {
+    //     method: "POST",
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ message, signature }),
+    //     credentials: 'include'
+    // });
+    // console.log(await res.text());
 }
 
-async function getInformation() {
-    const res = await fetch(`${BACKEND_ADDR}/personal_information`, {
-        credentials: 'include',
-    });
-    console.log(await res.text());
-}
+// async function getInformation() {
+//     const res = await fetch(`${BACKEND_ADDR}/personal_information`, {
+//         credentials: 'include',
+//     });
+//     console.log(await res.text());
+// }
 
 const connectWalletBtn = document.getElementById('connectWalletBtn');
 const siweBtn = document.getElementById('siweBtn');
-const infoBtn = document.getElementById('infoBtn');
+// const infoBtn = document.getElementById('infoBtn');
 connectWalletBtn.onclick = connectWallet;
 siweBtn.onclick = signInWithEthereum;
-infoBtn.onclick = getInformation;
+// infoBtn.onclick = getInformation;
